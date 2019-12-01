2bf21a27b481d5a9ad4455d477ca6d39fa8898cb
Relintai <relintai@gmail.com>
2019-12-01 23:21:17 +0100

    ESS update.

--- game/autoload/PhysicsQuery.gd
+++ game/autoload/PhysicsQuery.gd
@@ -1,14 +1,30 @@
-extends Spatial
+extends Node
 
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+# Copyright (c) 2019 Péter Magyar
 
-func _ready():
-	set_physics_process(true)
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 
-func _physics_process(delta):
-	pass
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+#func _ready():
+#	set_physics_process(true)
+#
+#func _physics_process(delta):
+#	pass
 
 #func queue_raycast():
 #	var space_state : PhysicsDirectSpaceState = get_world().direct_space_state
