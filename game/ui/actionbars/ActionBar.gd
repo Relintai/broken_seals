582f2298f25676ff9c1043a0f3d9527189bc6708
Relintai <relintai@gmail.com>
2019-12-07 19:01:34 +0100

    Actionbars are now using built in control functionality for layout. Also Vsync is on now.

--- game/ui/actionbars/ActionBar.gd
+++ game/ui/actionbars/ActionBar.gd
@@ -8,28 +8,10 @@ export (PackedScene) var action_bar_entry_scene
 export (NodePath) var child_container_path
 var child_container
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	child_container = get_node(child_container_path)
-#	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 func set_actionbar_entry(action_bar_entry : ActionBarEntry, player: Entity) -> void:
-	#rect_size = Vector2(action_bar_entry.size, (action_bar_entry.size) * action_bar_entry.slot_num)
-	#set_anchors_preset(Control.PRESET_BOTTOM_RIGHT, false)
-	#margin_left = action_bar_entry.action_bar_id * -(action_bar_entry.size + 5) - 10
-	
-	margin_top = - ((action_bar_entry.size) * action_bar_entry.slot_num)
-	#margin_bottom = 0
-	margin_right = -((action_bar_entry.action_bar_id - 1) * action_bar_entry.size)
-	margin_left = -((action_bar_entry.action_bar_id) * action_bar_entry.size)
 
 	for i in range(action_bar_entry.slot_num):
 		var b : ActionBarButtonEntry = action_bar_entry.get_button_for_slotid(i)
