c45c7eaa9a7d76ac9f3d15943fc1bb1e3cf8ff39
Relintai <relintai@gmail.com>
2020-02-17 20:51:49 +0100

    The new tool can now save, also improved it's code, and fixed a few bugs.

--- game/texture_tools/LayeredTextureMaker.gd
+++ game/texture_tools/LayeredTextureMaker.gd
@@ -2,8 +2,8 @@ tool
 extends Node2D
 class_name LayeredTextureMaker
 
-export(Vector2) var texture_size = Vector2(16, 16) setget set_tex_size
-export(String) var texture_name = ""
+export(Vector2) var texture_size : Vector2 = Vector2(16, 16) setget set_tex_size
+export(Texture) var texture : Texture = null
 
 export(float) var refresh_rate : float = 2.0
 export(bool) var save : bool = false setget save_set
@@ -12,46 +12,39 @@ var preview_node : Sprite = null
 var viewport : Viewport = null
 
 var last_refresh : float = 0
+var queued_save : bool = false
+
+const DEBUG_TREE : bool = false
 
 func _enter_tree():
+	if not Engine.editor_hint:
+		return
+		
 	set_meta("_edit_lock_", true)
-	emit_signal("item_lock_status_changed")
+	#emit_signal("item_lock_status_changed")
 	create_preview()
+	set_process(true)
 	
 func create_preview():
 	for c in get_children():
 		if c.name == "Preview":
-			preview_node = c
-			
-			for ch in preview_node.get_children():
-				if ch is Viewport:
-					viewport = ch as Viewport
-					break
-				
+			c.name = "------"
+			c.queue_free()
 			break
-
-	if viewport != null:
-		viewport.queue_free()
-		
-	if preview_node != null:
-		preview_node.queue_free()
-		
 		
 	preview_node = Sprite.new() as Sprite
 	preview_node.name = "Preview"
 	preview_node.centered = false
 	add_child(preview_node)
 		
-	#don't, so it won't show up in the inspector
-	#if Engine.editor_hint:
-	#	preview_node.owner = get_tree().edited_scene_root
+	if Engine.editor_hint and DEBUG_TREE:
+		preview_node.owner = get_tree().edited_scene_root
 	
 	viewport = Viewport.new()
 	preview_node.add_child(viewport)
 	
-	#don't, so it won't show up in the inspector
-	#if Engine.editor_hint:
-	#	viewport.owner = get_tree().edited_scene_root
+	if Engine.editor_hint and DEBUG_TREE:
+		viewport.owner = get_tree().edited_scene_root
 		
 	viewport.set_vflip(true)
 	viewport.set_size(texture_size)
@@ -61,6 +54,9 @@ func create_preview():
 	set_process(true)
 
 func _process(delta):
+	if (queued_save):
+		save()
+	
 	#not the best solution, but works for now
 	last_refresh += delta
 	
@@ -71,16 +67,29 @@ func _process(delta):
 			ch.queue_free()
 			
 		for ch in get_children():
-			viewport.add_child(ch.duplicate())
+			if ch.name == "Preview":
+				continue
+			
+			var n = ch.duplicate()
+			viewport.add_child(n)
+			
+			if Engine.editor_hint and DEBUG_TREE:
+				n.owner = get_tree().edited_scene_root
 			
 		if get_children().has(preview_node) and get_child(get_child_count() - 1) != preview_node:
 			move_child(preview_node, get_child_count() - 1)
 			
+		queued_save = true
+			
 
 func save() -> void:
-	if texture_name == "":
-		print("Set a name for your texture!")
+	queued_save = false
+	
+	if texture == null:
+		#print("LayeredTextureMaker: Set a target texture!")
 		return
+		
+	texture.set_data(viewport.get_texture().get_data())
 	
 func preview_refresh() -> void:
 	for ch in viewport.get_children():
