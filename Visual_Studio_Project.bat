0f35523183e7a60e6de906fe6965da974bda8d46
Relintai <relintai@gmail.com>
2020-03-24 17:01:29 +0100

    Removed one more unneeded script.

--- Visual_Studio_Project.bat
+++
@@ -1,8 +0,0 @@
-
-rem call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd6
-
-cd ./engine
-
-rem scons -j6 platform=windows
-
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64 && scons -j6 p=windows target=release_debug vsproj=yes voxel_mem_tools=no entity_mem_tools=no
