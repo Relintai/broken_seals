768af776e41703f624109b607079a6333bc54581
Relintai <relintai@gmail.com>
2020-04-04 17:29:23 +0200

    Update every module, to get the prop refactors.

--- game/voxelman/world/TVVoxelChunk.gd
+++ game/voxelman/world/TVVoxelChunk.gd
@@ -30,10 +30,6 @@ var _textures : Array
 var _prop_material : SpatialMaterial
 var _entities_spawned : bool
 
-#func _enter_tree():
-#	create_debug_immediate_geometry()
-	
-	
 func _create_meshers():
 	var mesher : TVVoxelMesher = TVVoxelMesher.new()
 	mesher.base_light_value = 0.45
@@ -51,145 +47,6 @@ func _create_meshers():
 	_prop_texture_packer.background_color = Color(0, 0, 0, 1)
 	_prop_texture_packer.texture_flags = Texture.FLAG_MIPMAPS
 
-func spawn_prop_entities(parent_transform : Transform, prop : PropData):
-	for i in range(prop.get_prop_count()):
-		var p : PropDataEntry = prop.get_prop(i)
-	
-		if p is PropDataEntity:
-			var pentity : PropDataEntity = p as PropDataEntity
-			
-			if pentity.entity_data_id != 0:
-				Entities.spawn_mob(pentity.entity_data_id, pentity.level, parent_transform.origin)
-
-						
-		if p is PropDataProp and p.prop != null:
-			var vmanpp : PropDataProp = p as PropDataProp
-			
-			spawn_prop_entities(get_prop_mesh_transform(parent_transform * p.transform, vmanpp.snap_to_mesh, vmanpp.snap_axis), p.prop)
-
-func build_phase_prop_mesh() -> void:
-	for i in range(get_mesher_count()):
-		get_mesher(i).reset()
-
-	if get_prop_count() == 0:
-		next_phase()
-		return
-		
-	if has_meshes(MESH_INDEX_PROP, MESH_TYPE_INDEX_MESH):
-		create_meshes(MESH_INDEX_PROP, lod_num + 1)
-		
-#	if _prop_material == null:
-#		_prop_material = SpatialMaterial.new()
-#		_prop_material.flags_vertex_lighting = true
-#		_prop_material.vertex_color_use_as_albedo = true
-#		_prop_material.params_specular_mode = SpatialMaterial.SPECULAR_DISABLED
-#		_prop_material.metallic = 0
-
-		VisualServer.instance_geometry_set_material_override(get_mesh_rid_index(MESH_INDEX_PROP, MESH_TYPE_INDEX_MESH_INSTANCE, 0), library.get_prop_material(0).get_rid())
-		
-		for i in range(get_mesher_count()):
-			get_mesher(i).material = _prop_material
-		
-	for i in range(get_prop_count()):
-		var prop : VoxelChunkPropData = get_prop(i)
-		
-		if prop.mesh != null and prop.mesh_texture != null:
-			var at : AtlasTexture = _prop_texture_packer.add_texture(prop.mesh_texture)
-			_textures.append(at)
-			
-		if prop.prop != null:
-			prop.prop.add_textures_into(_prop_texture_packer)
-	
-	if _prop_texture_packer.get_texture_count() > 0:
-		_prop_texture_packer.merge()
-		
-		_prop_material.albedo_texture = _prop_texture_packer.get_generated_texture(0)
-		
-	for i in range(get_prop_count()):
-		var prop : VoxelChunkPropData = get_prop(i)
-		
-		if prop.mesh != null:
-			var t : Transform = get_prop_transform(prop, prop.snap_to_mesh, prop.snap_axis)
-			
-			for i in range(get_mesher_count()):
-				prop.prop.add_meshes_into(get_mesher(i), _prop_texture_packer, t, voxel_world)
-			
-		if prop.prop != null:
-			var vmanpp : PropData = prop.prop as PropData
-			var t : Transform = get_prop_transform(prop, vmanpp.snap_to_mesh, vmanpp.snap_axis)
-			
-			for i in range(get_mesher_count()):
-				prop.prop.add_meshes_into(get_mesher(i), _prop_texture_packer, t, voxel_world)
-
-	for i in range(get_mesher_count()):
-		get_mesher(i).bake_colors(self)
-		get_mesher(i).build_mesh_into(get_mesh_rid_index(MESH_INDEX_PROP, MESH_TYPE_INDEX_MESH, 0))
-		get_mesher(i).material = null
-		
-	if not _entities_spawned:
-		for i in range(get_prop_count()):
-			var prop : VoxelChunkPropData = get_prop(i)
-			
-			if prop.prop != null:
-				spawn_prop_entities(get_prop_transform(prop, false, Vector3(0, -1, 0)), prop.prop)
-	
-	next_phase()
-	
-func build_phase_lights() -> void:
-	var vl : VoxelLight = VoxelLight.new()
-	
-	for i in range(get_prop_count()):
-		var prop : VoxelChunkPropData = get_prop(i)
-		
-		if prop.light == null and prop.prop == null:
-			continue
-		
-		var t : Transform = get_prop_transform(prop, prop.snap_to_mesh, prop.snap_axis)
-		
-		if prop.light != null:
-			var pl : PropDataLight = prop.light
-			
-			vl.set_world_position(prop.x + position_x * size_x, prop.y +  position_y * size_y, prop.z +  position_z * size_z)
-			vl.color = pl.light_color
-			vl.size = pl.light_size
-
-			bake_light(vl)
-			
-		if prop.prop != null:
-			prop.prop.add_prop_lights_into(self, t, true)
-
-func get_prop_transform(prop : VoxelChunkPropData, snap_to_mesh: bool, snap_axis: Vector3) -> Transform:
-	var pos : Vector3 = Vector3(prop.x * voxel_scale, prop.y * voxel_scale, prop.z * voxel_scale)
-	
-	var t : Transform = Transform(Basis(prop.rotation).scaled(prop.scale), pos)
-
-	if snap_to_mesh:
-		var global_pos : Vector3 = voxel_world.to_global(t.origin)
-		var world_snap_axis : Vector3 = voxel_world.to_global(t.xform(snap_axis))
-		var world_snap_dir : Vector3 = (world_snap_axis - global_pos) * 100
-		
-		var space_state : PhysicsDirectSpaceState = voxel_world.direct_space_state
-		var result : Dictionary = space_state.intersect_ray(global_pos - world_snap_dir, global_pos + world_snap_dir, [], 1)
-		
-		if result.size() > 0:
-			t.origin = voxel_world.to_local(result["position"])
-	
-	return t
-	
-func get_prop_mesh_transform(base_transform : Transform, snap_to_mesh: bool, snap_axis: Vector3) -> Transform:
-	if snap_to_mesh:
-		var pos : Vector3 = voxel_world.to_global(base_transform.origin)
-		var world_snap_axis : Vector3 = voxel_world.to_global(base_transform.xform(snap_axis))
-		var world_snap_dir : Vector3 = (world_snap_axis - pos) * 100
-		
-		var space_state : PhysicsDirectSpaceState = voxel_world.get_world().direct_space_state
-		var result : Dictionary = space_state.intersect_ray(pos - world_snap_dir, pos + world_snap_dir, [], 1)
-		
-		if result.size() > 0:
-			base_transform.origin = voxel_world.to_local(result["position"])
-
-	return base_transform
-
 func _build_phase(phase):
 	if phase == VoxelChunkDefault.BUILD_PHASE_SETUP:
 		._build_phase(phase)
@@ -198,8 +55,11 @@ func _build_phase(phase):
 		clear_baked_lights()
 		generate_random_ao()
 		bake_lights()
+		
+		next_phase()
+		
 		#set_physics_process_internal(true)
-		active_build_phase_type = VoxelChunkDefault.BUILD_PHASE_TYPE_PHYSICS_PROCESS
+#		active_build_phase_type = VoxelChunkDefault.BUILD_PHASE_TYPE_PHYSICS_PROCESS
 		return
 #	elif phase == VoxelChunkDefault.BUILD_PHASE_TERRARIN_MESH:
 #		for i in range(get_mesher_count()):
@@ -297,10 +157,6 @@ func _build_phase(phase):
 #		next_phase();
 #
 #		return
-	elif phase == VoxelChunkDefault.BUILD_PHASE_PROP_MESH:
-#		set_physics_process_internal(true)
-		active_build_phase_type = VoxelChunkDefault.BUILD_PHASE_TYPE_PHYSICS_PROCESS
-		return
 	else:
 		._build_phase(phase)
 
@@ -328,19 +184,4 @@ func generate_random_ao() -> void:
 					val = -val
 
 				set_voxel(int(val * 255.0), x, y, z, VoxelChunkDefault.DEFAULT_CHANNEL_RANDOM_AO)
-
-func _build_phase_physics_process(phase):
-	if current_build_phase == VoxelChunkDefault.BUILD_PHASE_LIGHTS:
-		build_phase_lights()
-#		set_physics_process_internal(false)
-		active_build_phase_type = VoxelChunkDefault.BUILD_PHASE_TYPE_NORMAL
-		next_phase()
-		
-	elif current_build_phase == VoxelChunkDefault.BUILD_PHASE_PROP_MESH:
-		build_phase_prop_mesh()
-#		set_physics_process_internal(false)
-		active_build_phase_type = VoxelChunkDefault.BUILD_PHASE_TYPE_NORMAL
-		next_phase()
-	else:
-		._build_phase_physics_process(phase)
 
