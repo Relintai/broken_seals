a37e8f5976f99b50f0af2c8616c07650b8477c43
Relintai <relintai@gmail.com>
2020-08-04 19:29:16 +0200

    Added weapon damage stats. Also work on the character window.

--- game/ui/windows/CharacterWindow.gd
+++ game/ui/windows/CharacterWindow.gd
@@ -3,9 +3,21 @@ extends PanelContainer
 export(NodePath) var opener_button_path : NodePath
 var opener_button : BaseButton
 
+export(NodePath) var container_path : NodePath
+var container : Node
+
+var _player : Entity
+
 func _ready():
 	opener_button = get_node_or_null(opener_button_path) as BaseButton
+	container = get_node(container_path)
 
+func set_player(p_player: Entity) -> void:
+	_player = p_player
+	
+	for c in container.get_children():
+		if c.has_method("set_player"):
+			c.set_player(_player)
 
 func _on_CharacterButton_toggled(button_pressed):
 	if button_pressed:
