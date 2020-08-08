aaf1458304c6929c8d8cad063d961961bcc833a1
Relintai <relintai@gmail.com>
2020-08-09 00:00:15 +0200

    Added simple logging to the in-container build script.

--- podman_build_all.sh
+++ podman_build_all.sh
@@ -11,24 +11,24 @@ fi
 project_root=$(pwd)/
 img_version=bs
 
-#mkdir -p logs
+mkdir -p logs
 
-$podman run -v ${project_root}:/root/project -w /root/project godot-windows:${img_version} scons bew -j4
-$podman run -v ${project_root}:/root/project -w /root/project godot-windows:${img_version} scons bewd -j4
-$podman run -v ${project_root}:/root/project -w /root/project godot-windows:${img_version} scons bw -j4
-$podman run -v ${project_root}:/root/project -w /root/project godot-windows:${img_version} scons bwr -j4
+$podman run -v ${project_root}:/root/project -w /root/project godot-windows:${img_version} scons bew -j4 . 2>&1 | tee logs/bew.log
+$podman run -v ${project_root}:/root/project -w /root/project godot-windows:${img_version} scons bewd -j4 . 2>&1 | tee logs/bewd.log
+$podman run -v ${project_root}:/root/project -w /root/project godot-windows:${img_version} scons bw -j4 . 2>&1 | tee logs/bw.log
+$podman run -v ${project_root}:/root/project -w /root/project godot-windows:${img_version} scons bwr -j4 . 2>&1 | tee logs/bwr.log
 
-$podman run -v ${project_root}:/root/project -w /root/project godot-linux:${img_version} scons bel -j4
-$podman run -v ${project_root}:/root/project -w /root/project godot-linux:${img_version} scons beld -j4
-$podman run -v ${project_root}:/root/project -w /root/project godot-linux:${img_version} scons bl -j4
-$podman run -v ${project_root}:/root/project -w /root/project godot-linux:${img_version} scons blr -j4
+$podman run -v ${project_root}:/root/project -w /root/project godot-linux:${img_version} scons bel -j4 . 2>&1 | tee logs/bel.log
+$podman run -v ${project_root}:/root/project -w /root/project godot-linux:${img_version} scons beld -j4 . 2>&1 | tee logs/beld.log
+$podman run -v ${project_root}:/root/project -w /root/project godot-linux:${img_version} scons bl -j4 . 2>&1 | tee logs/bl.log
+$podman run -v ${project_root}:/root/project -w /root/project godot-linux:${img_version} scons blr -j4 . 2>&1 | tee logs/blr.log
 
 
-$podman run -v ${project_root}:/root/project -w /root/project godot-javascript:${img_version} scons bj -j4
-$podman run -v ${project_root}:/root/project -w /root/project godot-javascript:${img_version} scons bjr -j4
+$podman run -v ${project_root}:/root/project -w /root/project godot-javascript:${img_version} scons bj -j4 . 2>&1 | tee logs/bj.log
+$podman run -v ${project_root}:/root/project -w /root/project godot-javascript:${img_version} scons bjr -j4 . 2>&1 | tee logs/bjr.log
 
-$podman run -v ${project_root}:/root/project -w /root/project godot-android:${img_version} scons ba -j4
-$podman run -v ${project_root}:/root/project -w /root/project godot-android:${img_version} scons bar -j4
+$podman run -v ${project_root}:/root/project -w /root/project godot-android:${img_version} scons ba -j4 . 2>&1 | tee logs/ba.log
+$podman run -v ${project_root}:/root/project -w /root/project godot-android:${img_version} scons bar -j4 . 2>&1 | tee logs/bar.log
 
 # $podman run -v ${project_root}:/root/project -i -w /root/project -t godot-windows:${img_version} scons bew -j4
 
