0621a5da3ba2c04126a32426275113da877e039b
Relintai <relintai@protonmail.com>
2021-04-17 12:27:47 +0200

    Fix error spam on test generation.

--- game/tw/new_terramanlevelgenerator.gd
+++ game/tw/new_terramanlevelgenerator.gd
@@ -8,6 +8,6 @@ func _generate_chunk(chunk: TerraChunk) -> void:
 	
 	var s : OpenSimplexNoise = OpenSimplexNoise.new()
 	
-	for x in range(chunk.data_size_x):
-		for z in range(chunk.data_size_z):
+	for x in range(-chunk.margin_start, chunk.size_x + chunk.margin_end):
+		for z in range(-chunk.margin_start, chunk.size_x + chunk.margin_end):
 			chunk.set_voxel(s.get_noise_2d(x, z) * 10.0, x, z, TerraChunkDefault.DEFAULT_CHANNEL_ISOLEVEL)
