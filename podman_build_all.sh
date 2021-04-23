011d72e9c0743701f51b5306c730d59398fdced0
Relintai <relintai@protonmail.com>
2021-04-23 21:05:29 +0200

    Add _strip build word to the podman builds. Also the same script now won't build debug versions.

--- podman_build_all.sh
+++ podman_build_all.sh
@@ -13,22 +13,22 @@ img_version=bs
 
 mkdir -p logs
 
-$podman run -v ${project_root}:/root/project -w /root/project godot-windows:${img_version} scons bew -j4 . 2>&1 | tee logs/bew.log
-$podman run -v ${project_root}:/root/project -w /root/project godot-windows:${img_version} scons bewd -j4 . 2>&1 | tee logs/bewd.log
-$podman run -v ${project_root}:/root/project -w /root/project godot-windows:${img_version} scons bw -j4 . 2>&1 | tee logs/bw.log
-$podman run -v ${project_root}:/root/project -w /root/project godot-windows:${img_version} scons bwr -j4 . 2>&1 | tee logs/bwr.log
+$podman run -v ${project_root}:/root/project -w /root/project godot-windows:${img_version} scons bew_strip -j4 . 2>&1 | tee logs/bew.log
+#$podman run -v ${project_root}:/root/project -w /root/project godot-windows:${img_version} scons bewd_strip -j4 . 2>&1 | tee logs/bewd.log
+$podman run -v ${project_root}:/root/project -w /root/project godot-windows:${img_version} scons bw_strip -j4 . 2>&1 | tee logs/bw.log
+$podman run -v ${project_root}:/root/project -w /root/project godot-windows:${img_version} scons bwr_strip -j4 . 2>&1 | tee logs/bwr.log
 
-$podman run -v ${project_root}:/root/project -w /root/project godot-linux:${img_version} scons bel -j4 . 2>&1 | tee logs/bel.log
-$podman run -v ${project_root}:/root/project -w /root/project godot-linux:${img_version} scons beld -j4 . 2>&1 | tee logs/beld.log
-$podman run -v ${project_root}:/root/project -w /root/project godot-linux:${img_version} scons bl -j4 . 2>&1 | tee logs/bl.log
-$podman run -v ${project_root}:/root/project -w /root/project godot-linux:${img_version} scons blr -j4 . 2>&1 | tee logs/blr.log
+$podman run -v ${project_root}:/root/project -w /root/project godot-linux:${img_version} scons bel_strip -j4 . 2>&1 | tee logs/bel.log
+#$podman run -v ${project_root}:/root/project -w /root/project godot-linux:${img_version} scons beld_strip -j4 . 2>&1 | tee logs/beld.log
+$podman run -v ${project_root}:/root/project -w /root/project godot-linux:${img_version} scons bl_strip -j4 . 2>&1 | tee logs/bl.log
+$podman run -v ${project_root}:/root/project -w /root/project godot-linux:${img_version} scons blr_strip -j4 . 2>&1 | tee logs/blr.log
 
 
-$podman run -v ${project_root}:/root/project -w /root/project godot-javascript:${img_version} scons bj -j4 . 2>&1 | tee logs/bj.log
-$podman run -v ${project_root}:/root/project -w /root/project godot-javascript:${img_version} scons bjr -j4 . 2>&1 | tee logs/bjr.log
+$podman run -v ${project_root}:/root/project -w /root/project godot-javascript:${img_version} scons bj_strip -j4 . 2>&1 | tee logs/bj.log
+$podman run -v ${project_root}:/root/project -w /root/project godot-javascript:${img_version} scons bjr_strip -j4 . 2>&1 | tee logs/bjr.log
 
-$podman run -v ${project_root}:/root/project -w /root/project godot-android:${img_version} scons ba -j4 . 2>&1 | tee logs/ba.log
-$podman run -v ${project_root}:/root/project -w /root/project godot-android:${img_version} scons bar -j4 . 2>&1 | tee logs/bar.log
+$podman run -v ${project_root}:/root/project -w /root/project godot-android:${img_version} scons ba_strip -j4 . 2>&1 | tee logs/ba.log
+$podman run -v ${project_root}:/root/project -w /root/project godot-android:${img_version} scons bar_strip -j4 . 2>&1 | tee logs/bar.log
 
 # $podman run -v ${project_root}:/root/project -i -w /root/project -t godot-windows:${img_version} scons bew -j4
 
