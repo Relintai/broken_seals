d3d61d34b333efa5750431bf84e3f43ac0307681
Relintai <relintai@protonmail.com>
2021-04-27 18:04:42 +0200

    Remove -v flag from the game editor script.

--- ged.sh
+++ ged.sh
@@ -3,4 +3,4 @@
 cp -u ./engine/bin/godot.x11.opt.tools.64 ./engine/bin/run.godot.x11.opt.tools.64
 
 export LD_LIBRARY_PATH=`pwd`/engine/bin/
-./engine/bin/run.godot.x11.opt.tools.64 -v -e --path ./game/
+./engine/bin/run.godot.x11.opt.tools.64 -e --path ./game/
