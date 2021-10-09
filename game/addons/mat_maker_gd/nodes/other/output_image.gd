8758111bf6faa9c8cf5f22b4cd8ac87c85de1792
Relintai <relintai@protonmail.com>
2021-10-09 14:47:55 +0200

    Renamed output.gd to output_image.gd and implemented it's logic.

---
+++ game/addons/mat_maker_gd/nodes/other/output_image.gd
@@ -0,0 +1,42 @@
+tool
+extends MMNode
+
+var image : Resource
+
+var postfix : String = ""
+
+func _init_properties():
+	image = MMNodeUniversalProperty.new()
+	image.default_type = MMNodeUniversalProperty.MMNodeUniversalPropertyDefaultType.DEFAULT_TYPE_IMAGE
+	image.input_slot_type = MMNodeUniversalProperty.SlotTypes.SLOT_TYPE_UNIVERSAL
+	image.slot_name = "image"
+	
+	register_input_property(image)
+
+func _register_methods(mm_graph_node) -> void:
+	mm_graph_node.add_slot_texture_universal(image)
+	mm_graph_node.add_slot_line_edit(MMNodeUniversalProperty.SlotTypes.SLOT_TYPE_NONE, MMNodeUniversalProperty.SlotTypes.SLOT_TYPE_NONE, "get_postfix", "set_postfix", "postfix")
+
+func _render(material) -> void:
+	var img : Image = image.get_active_image()
+	
+	if !img:
+		return
+	
+	var matpath : String = material.get_path()
+	
+	if matpath == "":
+		return
+		
+	var matbn : String = matpath.get_basename()
+	var final_file_name : String = matbn + postfix + ".png"
+	
+	img.save_png(final_file_name)
+
+func get_postfix() -> String:
+	return postfix
+	
+func set_postfix(pf : String) -> void:
+	postfix = pf
+	
+	set_dirty(true)
