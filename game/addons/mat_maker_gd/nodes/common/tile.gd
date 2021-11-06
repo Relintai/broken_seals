0cb0559118bf36e4e865321caa30f98cc0846dd8
Relintai <relintai@protonmail.com>
2021-11-06 11:50:39 +0100

    Color tiler node for mat_maker_gd.

--- game/addons/mat_maker_gd/nodes/common/tile.gd
+++ game/addons/mat_maker_gd/nodes/common/tile.gd
@@ -630,191 +630,78 @@ const Commons = preload("res://addons/mat_maker_gd/nodes/common/commons.gd")
 #tiler_color.mmg
 #Tiles several occurences of an input image while adding randomness.
 
-#		"code": "vec3 $(name_uv)_random_color;\nvec4 $(name_uv)_tiled_output = tiler_$(name)($uv, vec2($tx, $ty), int($overlap), vec2(float($seed)), $(name_uv)_random_color);",
-#		"inputs": [
-#			{
-#				"default": "0.0",
-#				"function": true,
-#				"label": "",
-#				"longdesc": "The input image or atlas of 4 or 16 input images",
-#				"name": "in",
-#				"shortdesc": "Input",
-#				"type": "rgba"
-#			},
-#			{
-#				"default": "1.0",
-#				"function": true,
-#				"label": "",
-#				"longdesc": "The mask applied to the pattern",
-#				"name": "mask",
-#				"shortdesc": "Mask",
-#				"type": "f"
-#			}
-#		],
-#		"instance": "vec4 tiler_$(name)(vec2 uv, vec2 tile, int overlap, vec2 _seed, out vec3 random_color) {\n\tvec4 c = vec4(0.0);\n\tvec3 rc = vec3(0.0);\n\tvec3 rc1;\n\tfor (int dx = -overlap; dx <= overlap; ++dx) {\n\t\tfor (int dy = -overlap; dy <= overlap; ++dy) {\n\t\t\tvec2 pos = fract((floor(uv*tile)+vec2(float(dx), float(dy))+vec2(0.5))/tile-vec2(0.5));\n\t\t\tvec2 seed = rand2(pos+_seed);\n\t\t\trc1 = rand3(seed);\n\t\t\tpos = fract(pos+vec2($fixed_offset/tile.x, 0.0)*floor(mod(pos.y*tile.y, 2.0))+$offset*seed/tile);\n\t\t\tfloat mask = $mask(fract(pos+vec2(0.5)));\n\t\t\tif (mask > 0.01) {\n\t\t\t\tvec2 pv = fract(uv - pos)-vec2(0.5);\n\t\t\t\tseed = rand2(seed);\n\t\t\t\tfloat angle = (seed.x * 2.0 - 1.0) * $rotate * 0.01745329251;\n\t\t\t\tfloat ca = cos(angle);\n\t\t\t\tfloat sa = sin(angle);\n\t\t\t\tpv = vec2(ca*pv.x+sa*pv.y, -sa*pv.x+ca*pv.y);\n\t\t\t\tpv *= (seed.y-0.5)*2.0*$scale+1.0;\n\t\t\t\tpv /= vec2($scale_x, $scale_y);\n\t\t\t\tpv += vec2(0.5);\n\t\t\t\tpv = clamp(pv, vec2(0.0), vec2(1.0));\n\t\t\t\t$select_inputs\n\t\t\t\tvec4 n = $in.variation(pv, $variations ? seed.x : 0.0);\n\t\t\t\tseed = rand2(seed);\n\t\t\t\tfloat na = n.a*mask*(1.0-$opacity*seed.x);\n\t\t\t\tfloat a = (1.0-c.a)*(1.0*na);\n\t\t\t\tc = mix(c, n, na);\n\t\t\t\trc = mix(rc, rc1, n.a);\n\t\t\t}\n\t\t}\n\t}\n\trandom_color = rc;\n\treturn c;\n}\n",
-#		"outputs": [
-#			{
-#				"longdesc": "Shows the generated pattern",
-#				"rgba": "$(name_uv)_tiled_output",
-#				"shortdesc": "Output",
-#				"type": "rgba"
-#			},
-#			{
-#				"longdesc": "Shows a random color for each instance of the input image",
-#				"rgb": "$(name_uv)_random_color",
-#				"shortdesc": "Instance Map",
-#				"type": "rgb"
-#			}
-#		],
-#		"parameters": [
-#			{
-#				"control": "None",
-#				"default": 4,
-#				"label": "Tile X",
-#				"longdesc": "The number of columns of the tiles pattern",
-#				"max": 64,
-#				"min": 1,
-#				"name": "tx",
-#				"shortdesc": "Tile.x",
-#				"step": 1,
-#				"type": "float"
-#			},
-#			{
-#				"control": "None",
-#				"default": 4,
-#				"label": "Tile Y",
-#				"longdesc": "The number of rows of the tiles pattern",
-#				"max": 64,
-#				"min": 1,
-#				"name": "ty",
-#				"shortdesc": "Tile.y",
-#				"step": 1,
-#				"type": "float"
-#			},
-#			{
-#				"control": "None",
-#				"default": 1,
-#				"label": "Overlap",
-#				"longdesc": "The number of neighbour tiles an instance of the input image can overlap. Set this parameter to the lowest value that generates the expected result (where all instances are fully visible) to improve performance.",
-#				"max": 5,
-#				"min": 0,
-#				"name": "overlap",
-#				"shortdesc": "Overlap",
-#				"step": 1,
-#				"type": "float"
-#			},
-#			{
-#				"default": 0,
-#				"label": "Inputs",
-#				"longdesc": "The input type of the node:\n- 1: single image\n- 4: atlas of 4 images\n- 16: atlas of 16 images\nAtlases can be created using the Tile2x2 node.",
-#				"name": "select_inputs",
-#				"shortdesc": "Input",
-#				"type": "enum",
-#				"values": [
-#					{
-#						"name": "1",
-#						"value": " "
-#					},
-#					{
-#						"name": "4",
-#						"value": "pv = clamp(0.5*(pv+floor(rand2(seed)*2.0)), vec2(0.0), vec2(1.0));"
-#					},
-#					{
-#						"name": "16",
-#						"value": "pv = clamp(0.25*(pv+floor(rand2(seed)*4.0)), vec2(0.0), vec2(1.0));"
-#					}
-#				]
-#			},
-#			{
-#				"control": "None",
-#				"default": 1,
-#				"label": "Scale X",
-#				"longdesc": "The scale of input images on the X axis",
-#				"max": 2,
-#				"min": 0,
-#				"name": "scale_x",
-#				"shortdesc": "Scale.x",
-#				"step": 0.01,
-#				"type": "float"
-#			},
-#			{
-#				"control": "None",
-#				"default": 1,
-#				"label": "Scale Y",
-#				"longdesc": "The scale of input images on the Y axis",
-#				"max": 2,
-#				"min": 0,
-#				"name": "scale_y",
-#				"shortdesc": "Scale.y",
-#				"step": 0.01,
-#				"type": "float"
-#			},
-#			{
-#				"control": "None",
-#				"default": 0.5,
-#				"label": "Fixed Offset",
-#				"longdesc": "The relative offset of odd rows",
-#				"max": 1,
-#				"min": 0,
-#				"name": "fixed_offset",
-#				"shortdesc": "FixedOffset",
-#				"step": 0.01,
-#				"type": "float"
-#			},
-#			{
-#				"control": "None",
-#				"default": 0.5,
-#				"label": "Rnd Offset",
-#				"max": 1,
-#				"min": 0,
-#				"name": "offset",
-#				"step": 0.01,
-#				"type": "float"
-#			},
-#			{
-#				"control": "None",
-#				"default": 0,
-#				"label": "Rnd Rotate",
-#				"longdesc": "The random rotation applied to each image instance",
-#				"max": 180,
-#				"min": 0,
-#				"name": "rotate",
-#				"shortdesc": "RndRotate",
-#				"step": 0.1,
-#				"type": "float"
-#			},
-#			{
-#				"control": "None",
-#				"default": 0,
-#				"label": "Rnd Scale",
-#				"longdesc": "The random scale applied to each image instance",
-#				"max": 1,
-#				"min": 0,
-#				"name": "scale",
-#				"shortdesc": "RndScale",
-#				"step": 0.01,
-#				"type": "float"
-#			},
-#			{
-#				"control": "None",
-#				"default": 0.5,
-#				"label": "Rnd Opacity",
-#				"longdesc": "The random opacity applied to each image instance",
-#				"max": 1,
-#				"min": 0,
-#				"name": "opacity",
-#				"shortdesc": "RndOpacity",
-#				"step": 0.01,
-#				"type": "float"
-#			},
-#			{
-#				"default": false,
-#				"label": "Variations",
-#				"longdesc": "Check to tile variations of the input",
-#				"name": "variations",
-#				"shortdesc": "Variations",
-#				"type": "boolean"
-#			}
-#		],
+#vec3 $(name_uv)_random_color;\n
+#vec4 $(name_uv)_tiled_output = tiler_$(name)($uv, vec2($tx, $ty), int($overlap), vec2(float($seed)), $(name_uv)_random_color);
 
+#vec4 tiler_$(name)(vec2 uv, vec2 tile, int overlap, vec2 _seed, out vec3 random_color) {\n\t
+#	vec4 c = vec4(0.0);\n\t
+#	vec3 rc = vec3(0.0);\n\t
+#	vec3 rc1;\n\t
+#
+#	for (int dx = -overlap; dx <= overlap; ++dx) {\n\t\t
+#		for (int dy = -overlap; dy <= overlap; ++dy) {\n\t\t\t
+#			vec2 pos = fract((floor(uv*tile)+vec2(float(dx), float(dy))+vec2(0.5))/tile-vec2(0.5));\n\t\t\t
+#			vec2 seed = rand2(pos+_seed);\n\t\t\t
+#			rc1 = rand3(seed);\n\t\t\t
+#			pos = fract(pos+vec2($fixed_offset/tile.x, 0.0)*floor(mod(pos.y*tile.y, 2.0))+$offset*seed/tile);\n\t\t\t
+#			float mask = $mask(fract(pos+vec2(0.5)));\n\t\t\t
+#			if (mask > 0.01) {\n\t\t\t\t
+#				vec2 pv = fract(uv - pos)-vec2(0.5);\n\t\t\t\t
+#				seed = rand2(seed);\n\t\t\t\t
+#				float angle = (seed.x * 2.0 - 1.0) * $rotate * 0.01745329251;\n\t\t\t\t
+#				float ca = cos(angle);\n\t\t\t\t
+#				float sa = sin(angle);\n\t\t\t\t
+#				pv = vec2(ca*pv.x+sa*pv.y, -sa*pv.x+ca*pv.y);\n\t\t\t\t
+#				pv *= (seed.y-0.5)*2.0*$scale+1.0;\n\t\t\t\t
+#				pv /= vec2($scale_x, $scale_y);\n\t\t\t\t
+#				pv += vec2(0.5);\n\t\t\t\t
+#				pv = clamp(pv, vec2(0.0), vec2(1.0));\n\t\t\t\t
+#
+#				$select_inputs\n\t\t\t\t
+#
+#				vec4 n = $in.variation(pv, $variations ? seed.x : 0.0);\n\t\t\t\t
+#
+#				seed = rand2(seed);\n\t\t\t\t
+#				float na = n.a*mask*(1.0-$opacity*seed.x);\n\t\t\t\t
+#				float a = (1.0-c.a)*(1.0*na);\n\t\t\t\t
+#
+#				c = mix(c, n, na);\n\t\t\t\t
+#				rc = mix(rc, rc1, n.a);\n\t\t\t
+#			}\n\t\t
+#		}\n\t
+#	}\n\t
+#
+#	random_color = rc;\n\t
+#	return c;\n
+#}
+
+
+#Inputs:
+
+#in, rgba, default: 0, - The input image or atlas of 4 or 16 input images
+#Mask, float, default: 1, - The mask applied to the pattern
+
+#Outputs:
+#Output, float, Shows the generated pattern
+#$(name_uv)_tiled_output
+
+#Instance map, rgb, Shows a random color for each instance of the input image
+#$(name_uv)_random_color
+
+#select_inputs enum
+#1,  " "
+#4, "pv = clamp(0.5*(pv+floor(rand2(seed)*2.0)), vec2(0.0), vec2(1.0));"
+#16, "pv = clamp(0.25*(pv+floor(rand2(seed)*4.0)), vec2(0.0), vec2(1.0));"
+
+#Parameters:
+#tile, Vector2, default 4, min:1, max:64, step:1 - The number of columns of the tiles pattern
+#overlap, float, default 1, min 0, max 5, step 1 - The number of neighbour tiles an instance of the input image can overlap. Set this parameter to the lowest value that generates the expected result (where all instances are fully visible) to improve performance.
+#select_inputs (Inputs), enum, default 0, values 1, 4, 16
+#scale, Vector2, default 1, min:0, max:2, step:0.01 - "The scale of input images on the X axis
+#fixed_offset, float, default 0.5, min 0, max 1, step 0.01 - The relative offset of odd rows
+#offset (rnd_offset), float, default 0.5, min 0, max 1, step 0.01
+#rotate (rnd_rotate), float, default 0, min 0, max 180, step 0.1
+#scale (rnd_scale), float, default 0.5, min 0, max 1, step 0.01 - The random scale applied to each image instance
+#opacity (rnd_opacity), float, default 0, min 0, max 1, step 0.01 - The random greyscale value applied to each image instance
+#variations, bool, default false, (disabled) - Check to tile variations of the input
 
