c34b629b26bc2cf0b221d455f47d8ca1d2b9b89c
Relintai <relintai@protonmail.com>
2021-12-25 02:32:00 +0100

    Move World's setup to the World class.

--- game/addons/world_generator/ui/WorldTab.gd
+++ game/addons/world_generator/ui/WorldTab.gd
@@ -1,3 +1,14 @@
 tool
 extends HBoxContainer
 
+var edited_world
+
+func refresh() -> void:
+	$HSplitContainer/ResourcePropertyList.edit_resource(edited_world)
+	$VBoxContainer/DataList.set_edited_resource(edited_world)
+	$HSplitContainer/RectEditor.set_edited_resource(edited_world)
+	
+func set_wgworld(wgw : WorldGenWorld) -> void:
+	edited_world = wgw
+	
+	refresh()
