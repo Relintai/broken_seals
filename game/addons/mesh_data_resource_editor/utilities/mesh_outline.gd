71662367aea1f8ef3e3776e06bc0b2cb8d5f2a69
Relintai <relintai@protonmail.com>
2021-12-31 00:47:08 +0100

    Small tweaks to the mesh outline generator.

--- game/addons/mesh_data_resource_editor/utilities/mesh_outline.gd
+++ game/addons/mesh_data_resource_editor/utilities/mesh_outline.gd
@@ -6,8 +6,6 @@ var _mdr : MeshDataResource
 var lines : PoolVector3Array
 
 func setup(mdr : MeshDataResource) -> void:
-	lines.resize(0)
-	
 	_mdr = mdr
 
 	refresh()
@@ -24,11 +22,13 @@ func refresh():
 	if _mdr.array[ArrayMesh.ARRAY_VERTEX] == null || _mdr.array[ArrayMesh.ARRAY_INDEX] == null:
 		return
 	
-	var vertices : PoolVector3Array = _mdr.array[ArrayMesh.ARRAY_VERTEX]
-	var indices : PoolIntArray = _mdr.array[ArrayMesh.ARRAY_INDEX]
+	var arr : Array = _mdr.array
+	
+	var vertices : PoolVector3Array = arr[ArrayMesh.ARRAY_VERTEX]
+	var indices : PoolIntArray = arr[ArrayMesh.ARRAY_INDEX]
 	
 	if vertices.size() == 0:
-		vertices = _mdr.array[ArrayMesh.ARRAY_VERTEX]
+		return
 
 	if indices.size() % 3 == 0:
 		for i in range(0, len(indices), 3):
