6cce5c169a5d267b45db46a6101f873c8bb15768
Relintai <relintai@protonmail.com>
2022-01-10 10:08:09 +0100

    Make the pi build script rename the built executables.

--- build_pi.sh
+++ build_pi.sh
@@ -3,4 +3,10 @@ scons bel_latomic_strip_slim -j4
 scons bl_latomic_strip_slim -j4
 scons blr_latomic_strip_slim -j4
 
+rm -f ./engine/bin/godot.x11.pi4.opt.32 
+rm -f ./engine/bin/godot.x11.pi4.opt.debug.32
+rm -f ./engine/bin/godot.x11.pi4.opt.tools.32
 
+mv ./engine/bin/godot.x11.opt.32 ./engine/bin/godot.x11.pi4.opt.32 
+mv ./engine/bin/godot.x11.opt.debug.32 ./engine/bin/godot.x11.pi4.opt.debug.32
+mv ./engine/bin/godot.x11.opt.tools.32 ./engine/bin/godot.x11.pi4.opt.tools.32
