1637395eef6b00d3cd936b69bc6ae4f4b87ce1dd
Relintai <relintai@protonmail.com>
2022-01-18 22:22:32 +0100

    Set the seam's material to always on to.

--- game/addons/mesh_data_resource_editor/MDIGizmoPlugin.gd
+++ game/addons/mesh_data_resource_editor/MDIGizmoPlugin.gd
@@ -7,7 +7,7 @@ var plugin
 
 func _init():
 	create_material("main", Color(0.7, 0.7, 0.7))
-	create_material("seam", Color(1, 0, 0))
+	create_material("seam", Color(1, 0, 0), false, true)
 	create_handle_material("handles")
 
 func get_name():
