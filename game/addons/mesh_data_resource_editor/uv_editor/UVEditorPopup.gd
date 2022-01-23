5c9841c4806fabe451ae8b30cececec669176162
Relintai <relintai@protonmail.com>
2022-01-23 01:26:25 +0100

    Check whether the signals are already connected in uveditor's popup. This fixed an in-editor error message.

--- game/addons/mesh_data_resource_editor/uv_editor/UVEditorPopup.gd
+++ game/addons/mesh_data_resource_editor/uv_editor/UVEditorPopup.gd
@@ -2,8 +2,11 @@ tool
 extends ConfirmationDialog
 
 func _enter_tree():
-	connect("confirmed", self, "on_ok_pressed")
-	get_cancel().connect("pressed", self, "on_cancel_pressed")
+	if !is_connected("confirmed", self, "on_ok_pressed"):
+		connect("confirmed", self, "on_ok_pressed")
+	
+	if !get_cancel().is_connected("pressed", self, "on_cancel_pressed"):
+		get_cancel().connect("pressed", self, "on_cancel_pressed")
 	
 func on_ok_pressed() -> void:
 	$UVEditor.ok_pressed()
