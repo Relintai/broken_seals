3db96fec680a7b7a1cf48828e5cfd24b561fc7eb
Relintai <relintai@protonmail.com>
2022-01-26 18:04:30 +0100

    Brought in code from the DataManager singleton (which maybe should be renamed) to the data manager addon. Also disabled the addon's panel's _ready() for now, and enabled it.

--- game/addons/data_manager/panels/MainPanel.gd
+++ game/addons/data_manager/panels/MainPanel.gd
@@ -5,22 +5,19 @@ signal inspect_data
 
 export(PackedScene) var resource_scene : PackedScene
 export(PackedScene) var folder_entry_button_scene : PackedScene
-export(String) var base_folder : String = "res://modules/"
+export(String) var base_folder : String = "res://"
 export(NodePath) var main_container : NodePath
 export(NodePath) var folder_entry_container_path : NodePath
-
 
 var _main_container : Node
 var _resource_scene : Node
 var _folder_entry_container : Node
 
+var _modules : Array = Array()
+var _folders : Array = Array()
 
-var _folders : Array = [
-]
-
-func _ready():
+func _old_ready():
 	var dir : Directory = Directory.new()
-	
 	
 	if dir.file_exists("res://ess_data.json"):
 		var file : File = File.new()
@@ -31,14 +28,7 @@ func _ready():
 			_folders = parse_json(s)
 			
 			file.close()
-#	else:
-#		var file : File = File.new()
-#
-#		if file.open("res://ess_data.json", File.WRITE) == OK:
-#			file.store_string(to_json(_folders))
-#
-#			file.close()
-	
+
 	_main_container = get_node(main_container)
 	
 	_resource_scene = resource_scene.instance()
@@ -75,6 +65,57 @@ func _ready():
 	
 	set_tab(0)
 #	set_tab("test")
+
+
+func initialize_modules() -> void:
+	_modules.clear()
+	
+	load_modules_at("res://")
+	
+	_modules.sort_custom(ModulePathSorter, "sort_ascending")
+	
+	for module in _modules:
+		if module.has_method("load_module"):
+			module.load_module()
+	
+func load_modules_at(path : String) -> void:
+	var dir = Directory.new()
+	if dir.open(path) == OK:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if file_name == "." or file_name == "..":
+				file_name = dir.get_next()
+				continue
+
+			if dir.current_is_dir():
+				if path == "res://":
+					load_modules_at(path + file_name)
+				else:
+					load_modules_at(path + "/" + file_name)
+			else:
+				if file_name == "game_module.tres":
+					var res : Resource = null
+					
+					if path == "res://":
+						res = ResourceLoader.load(path + file_name)
+					else:
+						res = ResourceLoader.load(path + "/" + file_name)
+						
+					if res.enabled:
+						_modules.append(res)
+					
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path: " + path)
+
+
+class ModulePathSorter:
+	static func sort_ascending(a, b):
+		if a.resource_path < b.resource_path:
+			return true
+		return false
+
 
 func set_tab(tab_index : int) -> void:
 	hide_all()
