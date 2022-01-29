7a2649c0e5e1b1c7af3c56d39d033b7d9a513075
Relintai <relintai@protonmail.com>
2022-01-29 20:20:16 +0100

    Also add osx and javascript editor support for the make release script.

--- make_release.sh
+++ make_release.sh
@@ -35,12 +35,14 @@ zip -q ../release/broken_seals${version_snake_cased}_javascript.zip  ./broken_se
 zip -q ../release/broken_seals${version_snake_cased}_linux.zip  ./broken_seals${version_snake_cased}_linux/*
 zip -q ../release/broken_seals${version_snake_cased}_windows.zip  ./broken_seals${version_snake_cased}_windows/*
 zip -q ../release/broken_seals${version_snake_cased}_pi4.zip  ./broken_seals${version_snake_cased}_pi4/*
+zip -q ../release/broken_seals${version_snake_cased}_osx.zip  ./broken_seals${version_snake_cased}_osx/*
 
+# Editor
 zip -q ../release/editor_windows_bs${version_snake_cased}.zip  ./godot.bs${version}.windows.opt.tools.64.exe
 zip -q ../release/editor_linux_bs${version_snake_cased}.zip  ./godot.bs${version}.x11.opt.tools.64
 zip -q ../release/editor_pi4_bs${version_snake_cased}.zip  ./godot.bs${version}.x11.pi4.opt.tools.32
-
 cp  ./godot.bs${version}.javascript.opt.tools.zip ../release/editor_javascript_bs${version_snake_cased}.zip
+zip -q ../release/editor_osx_bs${version_snake_cased}.zip  ./godot.bs${version}.osx.opt.tools.zip
 
 zip -q ../release/export_templates_bs${version_snake_cased}.zip  ./export_templates_bs${version_snake_cased}/*
 
