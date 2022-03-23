d40af8d8fbcc89ad9cd08babd599fafa39d31de9
Relintai <relintai@protonmail.com>
2022-03-23 22:24:45 +0100

    Moved some of the release / build scripts to the engine.

--- tools/osx/lipo.sh
+++
@@ -1,6 +0,0 @@
-# Tools
-lipo -create  ../../pandemonium_engine/bin/pandemonium.osx.opt.tools.x86_64 ../../pandemonium_engine/bin/pandemonium.osx.opt.tools.arm64 -output ../../pandemonium_engine/bin/pandemonium.osx.opt.tools.universal
-
-# Export Templates
-lipo -create  ../../pandemonium_engine/bin/pandemonium.osx.opt.x86_64 ../../pandemonium_engine/bin/pandemonium.osx.opt.arm64 -output ../../pandemonium_engine/bin/pandemonium.osx.opt.universal
-lipo -create  ../../pandemonium_engine/bin/pandemonium.osx.opt.debug.x86_64 ../../pandemonium_engine/bin/pandemonium.osx.opt.debug.arm64 -output ../../pandemonium_engine/bin/pandemonium.osx.opt.debug.universal
