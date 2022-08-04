d53a1576576c89ec236d372991c0286d37e92944
Relintai <relintai@protonmail.com>
2022-08-05 00:56:01 +0200

    Set a usable initial zoom value in world generator's rect editors.

--- game/addons/world_generator/ui/RectEditor.gd
+++ game/addons/world_generator/ui/RectEditor.gd
@@ -1,6 +1,8 @@
 tool
 extends PanelContainer
 
+var last_edited_res : WorldGenBaseResource = null
+
 func _init():
 #	Control/EditorZoomWidget
 	pass
@@ -9,4 +11,15 @@ func set_plugin(plugin : EditorPlugin) -> void:
 	$ScrollContainer/MarginContainer/RectView.set_plugin(plugin)
 
 func set_edited_resource(res : WorldGenBaseResource):
+	if res && res != last_edited_res:
+		var r : Rect2 = res.get_rect()
+		
+		if r.size.x > 0:
+			var rsx : float = $ScrollContainer/MarginContainer.rect_size.x
+			var scale : float = rsx / r.size.x
+			
+			$Control/EditorZoomWidget.zoom = scale
+			
+		
+	
 	$ScrollContainer/MarginContainer/RectView.set_edited_resource(res)
