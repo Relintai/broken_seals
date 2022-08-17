82fb8e1efaee95c28a207719d65250970abdd54c
Relintai <relintai@protonmail.com>
2022-08-17 21:04:15 +0200

    Now the data manager addon uses a built in editor icon for it's top bar icon, so it won't get disabled when (re)importing the game project.

--- game/addons/data_manager/plugin.gd
+++ game/addons/data_manager/plugin.gd
@@ -2,9 +2,9 @@ tool
 extends EditorPlugin
 
 const DataManagerAddonSettings = preload("res://addons/data_manager/resources/data_manager_addon_settings.gd")
-
 const _main_panel : PackedScene = preload("res://addons/data_manager/panels/MainPanel.tscn")
-const _script_icon : Texture = preload("res://addons/data_manager/icons/icon_multi_line.png")
+
+var _script_icon : Texture = null
 
 var settings : DataManagerAddonSettings = null
 
@@ -34,6 +34,9 @@ func make_visible(visible):
 		_main_panel_instance.hide()
 
 func get_plugin_icon():
+	if !_script_icon:
+		_script_icon = get_editor_interface().get_base_control().get_theme_icon("ThemeSelectAll", "EditorIcons")
+		
 	return _script_icon
 
 func get_plugin_name():
